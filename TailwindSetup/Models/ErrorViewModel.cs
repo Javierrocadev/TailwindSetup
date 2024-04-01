1c5416e769006531e12f24e4b5c7443eb8930c65
Javier Roca <javierroca.dev@gmail.com>
2024-04-01 12:17:39 +0200

    Add project files.

---
+++ TailwindSetup/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TailwindSetup.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
